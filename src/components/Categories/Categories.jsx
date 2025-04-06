bed0a73d602a04535871ad57d7284a4d35f9c0b0
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-04-06 17:17:17 +0300

    updated categories links ui look

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -3,83 +3,77 @@ import { Link } from "react-router";
 
 const Categories = () => {
   return (
-    <div className="px-8 py-20 md:px-12 lg:px-28 bg-gray-100" id="categories">
-      <h2 className="text-3xl text-slate-800 font-semibold mb-2">Explore Popular Categories</h2>
-      <h3 className="text-lg text-gray-600">Find the perfect gear for your setup!</h3>
-      <div>
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 py-8">
-          <Link to="/keyboard_mice" className=" p-3 bg-gray-100 hover:bg-white  rounded-lg  flex items-center space-x-5">
+    <div className="p-8 md:px-12 lg:px-28 bg-gray-50" id="categories">
+      <h2 className="text-3xl text-slate-800 font-semibold mb-2 text-center">Explore Popular Categories</h2>
+      <h3 className="text-md text-gray-600 text-center mb-2">Find the perfect gear for your setup!</h3>
+      <div className="py-6">
+        <div className="flex flex-wrap items-center justify-center gap-8 ">
+          <Link to="/keyboard_mice" className=" bg-gray-100 hover:bg-white border p-2 border-red-500 rounded-full flex items-center space-x-2">
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#e02424" viewBox="0 0 256 256">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#e02424" viewBox="0 0 256 256">
                 <path d="M224,48H32A16,16,0,0,0,16,64V192a16,16,0,0,0,16,16H224a16,16,0,0,0,16-16V64A16,16,0,0,0,224,48Zm0,144H32V64H224V192Zm-16-64a8,8,0,0,1-8,8H56a8,8,0,0,1,0-16H200A8,8,0,0,1,208,128Zm0-32a8,8,0,0,1-8,8H56a8,8,0,0,1,0-16H200A8,8,0,0,1,208,96ZM72,160a8,8,0,0,1-8,8H56a8,8,0,0,1,0-16h8A8,8,0,0,1,72,160Zm96,0a8,8,0,0,1-8,8H96a8,8,0,0,1,0-16h64A8,8,0,0,1,168,160Zm40,0a8,8,0,0,1-8,8h-8a8,8,0,0,1,0-16h8A8,8,0,0,1,208,160Z"></path>
               </svg>
             </div>
             <div>
-              <p className="text-red-600 text-md font-semibold py-3">Keyboards & Mice</p>
-              <p className="text-xs text-gray-600"> Elevate your typing experience.</p>
+              <p className="text-red-600 text-sm font-semibold py-1">Keyboards & Mice</p>
             </div>
           </Link>
-          <Link to="/monitor" className="p-4 bg-gray-100 hover:bg-white  rounded-lg  flex items-center space-x-5">
+          <Link to="/monitor" className=" bg-gray-100 hover:bg-white border p-2 border-red-500 rounded-full flex items-center space-x-2">
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#e02424" viewBox="0 0 256 256">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#e02424" viewBox="0 0 256 256">
                 <path d="M208,40H48A24,24,0,0,0,24,64V176a24,24,0,0,0,24,24H208a24,24,0,0,0,24-24V64A24,24,0,0,0,208,40Zm8,136a8,8,0,0,1-8,8H48a8,8,0,0,1-8-8V64a8,8,0,0,1,8-8H208a8,8,0,0,1,8,8Zm-48,48a8,8,0,0,1-8,8H96a8,8,0,0,1,0-16h64A8,8,0,0,1,168,224Z"></path>
               </svg>
             </div>
             <div>
-              <p className="text-red-600 text-md font-semibold py-3">Monitors </p>
-              <p className="text-xs text-gray-600">Crystal-clear visuals for seamless work</p>
+              <p className="text-red-600 text-sm font-semibold">Monitors </p>
             </div>
           </Link>
-          <Link to="/headphones" className="p-4 bg-gray-100 hover:bg-white  rounded-lg  flex items-center space-x-5">
+          <Link to="/headphones" className=" bg-gray-100 hover:bg-white border p-2 border-red-500 rounded-full flex items-center space-x-2">
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#e02424" viewBox="0 0 256 256">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#e02424" viewBox="0 0 256 256">
                 <path d="M201.89,54.66A103.43,103.43,0,0,0,128.79,24H128A104,104,0,0,0,24,128v56a24,24,0,0,0,24,24H64a24,24,0,0,0,24-24V144a24,24,0,0,0-24-24H40.36A88,88,0,0,1,128,40h.67a87.71,87.71,0,0,1,87,80H192a24,24,0,0,0-24,24v40a24,24,0,0,0,24,24h16a24,24,0,0,0,24-24V128A103.41,103.41,0,0,0,201.89,54.66ZM64,136a8,8,0,0,1,8,8v40a8,8,0,0,1-8,8H48a8,8,0,0,1-8-8V136Zm152,48a8,8,0,0,1-8,8H192a8,8,0,0,1-8-8V144a8,8,0,0,1,8-8h24Z"></path>
               </svg>
             </div>
             <div>
               {" "}
-              <p className="text-red-600 text-md font-semibold py-3">Headphones</p>
-              <p className="text-xs text-gray-600"> Block out distractions, focus on code.</p>
+              <p className="text-red-600 text-sm font-semibold">Headphones</p>
             </div>
           </Link>
-          <Link to="/laptop" className="p-4 bg-gray-100 hover:bg-white  rounded-lg  flex items-center space-x-5">
+          <Link to="/laptop" className=" bg-gray-100 hover:bg-white border p-2 border-red-500 rounded-full flex items-center space-x-2">
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#e02424" viewBox="0 0 256 256">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#e02424" viewBox="0 0 256 256">
                 <path d="M232,168h-8V72a24,24,0,0,0-24-24H56A24,24,0,0,0,32,72v96H24a8,8,0,0,0-8,8v16a24,24,0,0,0,24,24H216a24,24,0,0,0,24-24V176A8,8,0,0,0,232,168ZM48,72a8,8,0,0,1,8-8H200a8,8,0,0,1,8,8v96H48ZM224,192a8,8,0,0,1-8,8H40a8,8,0,0,1-8-8v-8H224ZM152,88a8,8,0,0,1-8,8H112a8,8,0,0,1,0-16h32A8,8,0,0,1,152,88Z"></path>
               </svg>
             </div>
             <div>
-              <p className="text-red-600 text-md font-semibold py-3">Laptops</p>
-              <p className="text-xs text-gray-600"> Powerful, tech-friendly laptops for seamless work</p>
+              <p className="text-red-600 text-sm font-semibold">Laptops</p>
             </div>
           </Link>
-          <Link to="/desktop_pc" className="p-4 bg-gray-100 hover:bg-white  rounded-lg  flex items-center space-x-5">
+          <Link to="/desktop_pc" className="bg-gray-100 hover:bg-white border p-2 border-red-500 rounded-full flex items-center space-x-2">
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#e02424" viewBox="0 0 256 256">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#e02424" viewBox="0 0 256 256">
                 <path d="M216,72a8,8,0,0,1-8,8H176a8,8,0,0,1,0-16h32A8,8,0,0,1,216,72Zm-8,24H176a8,8,0,0,0,0,16h32a8,8,0,0,0,0-16Zm40-48V208a16,16,0,0,1-16,16H152a16,16,0,0,1-16-16V192H96v16h16a8,8,0,0,1,0,16H64a8,8,0,0,1,0-16H80V192H32A24,24,0,0,1,8,168V96A24,24,0,0,1,32,72H136V48a16,16,0,0,1,16-16h80A16,16,0,0,1,248,48ZM136,176V88H32a8,8,0,0,0-8,8v72a8,8,0,0,0,8,8Zm96,32V48H152V208h80Zm-40-40a12,12,0,1,0,12,12A12,12,0,0,0,192,168Z"></path>
               </svg>
             </div>
             <div>
               {" "}
-              <p className="text-red-600 text-md font-semibold py-3">Desktop PC</p>
-              <p className="text-xs text-gray-600"> Adjustable lighting to reduce eye strain and improve focus</p>
+              <p className="text-red-600 text-sm font-semibold">Desktop PC</p>
             </div>
           </Link>
-          <Link to="/cables_charger" className="p-4 bg-gray-100 hover:bg-white  rounded-lg  flex items-center space-x-5">
+          <Link to="/cables_charger" className=" bg-gray-100 hover:bg-white border p-2 border-red-500 rounded-full flex items-center space-x-2">
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#e02424" viewBox="0 0 256 256">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#e02424" viewBox="0 0 256 256">
                 <path d="M224,56H176V16a8,8,0,0,0-16,0V56H96V16a8,8,0,0,0-16,0V56H32.55C26.28,56,24,60.78,24,64a8,8,0,0,0,8,8H48v88a40,40,0,0,0,40,40h32v40a8,8,0,0,0,16,0V200h32a40,40,0,0,0,40-40V72h16a8,8,0,0,0,0-16ZM168,184H88a24,24,0,0,1-24-24V72H192v88A24,24,0,0,1,168,184Zm-17.42-60.56a8,8,0,0,1,.91,7.37l-12,32a8,8,0,0,1-15-5.62l8-21.19H112a8,8,0,0,1-7.49-10.81l12-32a8,8,0,1,1,15,5.62l-8,21.19H144A8,8,0,0,1,150.58,123.44Z"></path>
               </svg>
             </div>
             <div>
               {" "}
-              <p className="text-red-600 text-md font-semibold py-3"> Cables & Chargers</p>
-              <p className="text-xs text-gray-600"> Stay powered and connected</p>
+              <p className="text-red-600 text-sm font-semibold"> Cables & Chargers</p>
             </div>
           </Link>
-          <Link to="/desk_accessories" className="p-4 bg-gray-100 hover:bg-white  rounded-lg  flex items-center space-x-5">
+          <Link to="/desk_accessories" className=" bg-gray-100 hover:bg-white border p-2 border-red-500 rounded-full flex items-center space-x-2">
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" color="#E02424" fill="none">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="17" height="17" color="#E02424" fill="none">
                 <path d="M22 12H2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
                 <path
                   d="M20 17H16C14.1144 17 13.1716 17 12.5858 16.4142C12 15.8284 12 14.8856 12 13V12"
@@ -100,14 +94,13 @@ const Categories = () => {
               </svg>
             </div>
             <div>
-              <p className="text-red-600 text-md font-semibold py-3">Desk Accessories </p>
-              <p className="text-xs text-gray-600">Organize and optimize your workspace</p>
+              <p className="text-red-600 text-sm font-semibold">Desk Accessories </p>
             </div>
           </Link>
 
-          <Link to="/apparel" className="p-4 bg-gray-100 hover:bg-white  rounded-lg flex items-center space-x-5">
+          <Link to="/apparel" className=" bg-gray-100 hover:bg-white border p-2 border-red-500 rounded-full flex items-center space-x-2">
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" color="#E02424" fill="none">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="17" height="17" color="#E02424" fill="none">
                 <path
                   d="M5.99756 12V17.6841C5.99756 19.4952 5.99756 20.4008 6.58285 20.9635C7.24789 21.6028 9.6154 21.9785 11.9925 21.9991C14.3085 22.0192 16.6337 21.7022 17.4021 20.9635C17.9874 20.4008 17.9874 19.4952 17.9874 17.6841V12"
                   stroke="currentColor"
@@ -139,15 +132,12 @@ const Categories = () => {
               </svg>
             </div>
             <div>
-              <p className="text-red-600 text-md font-semibold py-3">Developer Apparel & Merch</p>
-              <p className="text-xs text-gray-600"> Show off your love for tech with stylish t-shirts, hoodies, and mugs</p>
+              <p className="text-red-600 text-sm font-semibold">Developer Apparel & Merch</p>
             </div>
           </Link>
         </div>
       </div>
-      <div>
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-5 py-8"></div>
-      </div>
+      <div></div>
     </div>
   );
 };
