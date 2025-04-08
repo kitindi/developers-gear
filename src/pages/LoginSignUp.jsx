6e6868b08ceb0316d44f136892c83191095674ab
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-04-08 12:03:52 +0300

    improved login & register page ui

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -58,9 +58,9 @@ const LoginSignUp = () => {
     <div className="px-4 md:px-12 lg:px-28 min-h-96 py-8 lg:py-4">
       <section class="">
         <div class="flex flex-col items-center justify-center px-2 mx-auto md:h-screen lg:py-0">
-          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div class="w-full bg-white rounded-lg  md:mt-0 sm:max-w-md xl:p-0">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-center  text-gray-900 md:text-2xl dark:text-white">Sign in to your account</h1>
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-center  text-gray-900 md:text-2xl">Sign in to your account</h1>
               <form class="space-y-4 md:space-y-6" method="POST" onSubmit={handleSubmit}>
                 <div>
                   <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -72,13 +72,13 @@ const LoginSignUp = () => {
                     id="email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5 focus:outline-none"
                     placeholder="name@company.com"
                     required=""
                   />
                 </div>
                 <div>
-                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  <label for="password" class="block mb-2 text-sm font-medium text-gray-900">
                     Password
                   </label>
                   <input
@@ -88,7 +88,7 @@ const LoginSignUp = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     placeholder="••••••••"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg  block w-full p-2.5 focus:outline-none"
                     required=""
                   />
                 </div>
